cb1a8ef57e8a25c31cfcaf7bce52f4f0c42b708b
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-14 23:34:51 +0000

    got image downloader working with hard coded url

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,8 +1,8 @@
 const request = require('request');
 const auth = require('./secrets.js')
+const fs = require('fs');
 
 function getRepoContributors(repoOwner, repoName, callback) {
-//https://api.github.com/repos/jquery/jquery/contributors
   let options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -27,6 +27,16 @@ function avatarURLS(err, userInfo){
     console.log(currUser.avatar_url);
   });
 }
+
+function downloadImageByURL(url, filePath){
+
+  request.get(url)
+  .on('error', err => {throw err})
+
+  .pipe(fs.createWriteStream(filePath), console.log("Download Complete!"));
+}
+
+// downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg");
 
 
 
