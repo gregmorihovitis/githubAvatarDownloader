729d34093d330b0006f956cfe22970b342158541
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-14 23:09:42 +0000

    printing a list of user avatar urls

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -3,19 +3,30 @@ const auth = require('./secrets.js')
 
 function getRepoContributors(repoOwner, repoName, callback) {
 //https://api.github.com/repos/jquery/jquery/contributors
-  var options = {
+  let options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authorization': "auth.githubToken"
     }
   };
+
+  request(options, function(err, res, body) {
+    let userData = JSON.parse(body);
+
+    callback(err, userData);
+    });
 }
 
 //Test Code
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+getRepoContributors("jquery", "jquery", avatarURLS);
+
+function avatarURLS(err, userInfo){
+  if(err) throw err;
+  userInfo.forEach(currUser => {
+    console.log(currUser.avatar_url);
+  });
+}
+
 
 
