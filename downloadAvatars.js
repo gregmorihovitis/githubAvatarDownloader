d2f6ffdacb8bad2f8960e8b08df68a5c68d5471b
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-14 22:08:59 +0000

    set up initial functions

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,1 +1,13 @@
 const request = require('request');
+
+function getRepoContributors(repoOwner, repoName, callback) {
+//https://api.github.com/repos/jquery/jquery/contributors
+}
+
+//Test Code
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+//gregmorihovitis:9976f474ccd142e881324a3aa9613e3d127512f9
