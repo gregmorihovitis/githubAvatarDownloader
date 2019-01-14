ee40784c7cb62a46aad839b23adeea2e229b662f
gregmorihovitis <gregmorihovitis@gmail.com>
2019-01-14 22:21:53 +0000

    added token request to main file

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -1,7 +1,15 @@
 const request = require('request');
+const auth = require('./secrets.js')
 
 function getRepoContributors(repoOwner, repoName, callback) {
 //https://api.github.com/repos/jquery/jquery/contributors
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': "auth.githubToken"
+    }
+  };
 }
 
 //Test Code
@@ -10,4 +18,4 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Result:", result);
 });
 
-//gregmorihovitis:9976f474ccd142e881324a3aa9613e3d127512f9
+
